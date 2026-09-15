ad9028900ff2b87bea28f7b0d642a596b87ef773
JaimeFerrer <145484716+JaimeFerrer@users.noreply.github.com>
2026-09-15 19:54:38 +0200

    Registra foto images/upload-1789494875590.jpg en data.js

--- js/data.js
+++ js/data.js
@@ -118,6 +118,7 @@ const PHOTOS = [
   { file: "images/aida-105.jpg", categories: ["tia"], caption: "" },
   { file: "images/aida-106.jpg", categories: ["tia", "guelos", "diego", "mama", "jametes"], caption: "" },
   { file: "images/aida-107.jpg", categories: ["guelos", "tia", "mama", "diego"], caption: "" },
+  { file: "images/upload-1789494875590.jpg", categories: ["jametes"], caption: "Cumple 2026" },
 ];
 
 // Nombres bonitos para mostrar en los botones de filtro.
