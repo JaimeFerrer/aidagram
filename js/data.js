b59898bdf5be16451eac1a7bbf683c6c34990ddf
JaimeFerrer <145484716+JaimeFerrer@users.noreply.github.com>
2026-09-15 20:11:31 +0200

    Elimina la foto images/aida-55.png de data.js

--- js/data.js
+++ js/data.js
@@ -62,7 +62,6 @@ const PHOTOS = [
   { file: "images/aida-50.png", categories: ["jametes"], caption: "" },
   { file: "images/aida-51.png", categories: ["jametes"], caption: "" },
   { file: "images/aida-52.png", categories: ["moni"], caption: "" },
-  { file: "images/aida-55.png", categories: ["mama"], caption: "" },
   { file: "images/aida-57.png", categories: ["mama"], caption: "" },
   { file: "images/aida-58.png", categories: ["jametes"], caption: "" },
   { file: "images/aida-59.png", categories: ["jametes", "mama"], caption: "" },
